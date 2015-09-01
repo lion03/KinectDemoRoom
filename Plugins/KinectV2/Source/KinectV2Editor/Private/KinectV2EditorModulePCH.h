f1e2185b6a11996eefac524c55c2b815f63d4ee6
Leon <lion032@gmail.com>
2015-09-01 09:33:48 +0300

    minor fixes

--- Plugins/KinectV2/Source/KinectV2Editor/Private/KinectV2EditorModulePCH.h
+++ Plugins/KinectV2/Source/KinectV2Editor/Private/KinectV2EditorModulePCH.h
@@ -5,5 +5,4 @@
 #include "AnimGraphDefinitions.h"
 #include "Kismet2/BlueprintEditorUtils.h"
 #include "AnimGraphNode_SkeletalControlBase.h"
-#include "AnimGraphNode_KinectV2Retarget.h"
 #include "KinectV2EditorModule.h"
